4d58465114ab068b504debd2fdd8185005e81ed3
Garrett Malmquist <garrett.malmquist@gmail.com>
2020-02-18 10:45:39 -0500

    Implementing sphere shape.

--- src/geom.rs
+++ src/geom.rs
@@ -1,4 +1,5 @@
 use crate::threed::{Basis3, Frame3, Pt3, Ray3, Vec3};
+use std::f64::{NAN, INFINITY};
 
 pub trait Shape {
     fn raycast(&self, ray: &Ray3) -> Option<RaycastHit>;
@@ -177,6 +178,80 @@ impl Edge {
     }
 }
 
+pub struct Sphere {
+    pub center: Pt3,
+    pub radius: f64,
+}
+
+impl Sphere {
+    pub fn new(center: Pt3, radius: f64) -> Sphere {
+        return Sphere { center, radius };
+    }
+}
+
+impl Shape for Sphere {
+    fn raycast(&self, ray: &Ray3) -> Option<RaycastHit> {
+        // Distance from (ray origin + direction * time) to center of sphere = radius.
+        // ||Q+Dt - C|| = r
+        // ||CQ + Dt|| = r
+        // ||CQ + Dt||**2 = r**2
+        // A vector's squared magnitude is equal to its dot-product with itself.
+        // (CQ + Dt)*(CQ + Dt) = r**2
+        // FOIL
+        // CQ*CQ + CQ*Dt + Dt*CQ + Dt*Dt = r**2
+        // CQ*CQ + 2 CQ*D t + D*D t**2 - r**2 = 0
+        // Quadratic formula
+        // a = D*D (t**2)
+        // b = 2 CQ*D (t)
+        // c = CQ*CQ - r**2
+        // t = (-b +|- sqrt(bb - 4ac))/(2a)
+        let a = ray.direction.mag2();
+        if a == 0. {
+            // Ray's direction has 0 magnitude, and will never reach the sphere.
+            return None;
+        }
+
+        let cq = ray.origin - self.center;
+        let b = 2. * (cq * ray.direction);
+        let c = (cq * cq) - self.radius * self.radius;
+
+        let radicand = b*b - 4.*a*c;
+        if radicand < 0. {
+            // Ray is not pointing at the sphere.
+            return None;
+        }
+
+        let radical = radicand.sqrt();
+
+        // We have 0-2 positive hits.
+        // Both negative: ray is pointing the opposite direction of the sphere
+        // Both positive: ray hits the sphere twice (goes through it and hits the back side).
+        // One positive, one negative: ray's origin is inside the sphere.
+        // Same number: ray is tangent to the sphere, grazing it a single point.
+        let t0 = (-b + radical) / (2. * a);
+        let t1 = (-b - radical) / (2. * a);
+
+        if t0 < 0. && t1 < 0. {
+            return None;
+        }
+
+        let distance = min_positive(t0, t1);
+        if distance < 0. {
+            return None
+        }
+        let point = ray.at(distance);
+        Some(RaycastHit {
+            distance,
+            point,
+            normal: (point - self.center).normalized(),
+        })
+    }
+
+    fn signed_distance(&self, pt: Pt3) -> f64 {
+        unimplemented!()
+    }
+}
+
 pub struct Mesh {
     pub polygons: Vec<Polygon>,
 }
@@ -189,7 +264,7 @@ impl Mesh {
 
 impl Shape for Mesh {
     fn raycast(&self, ray: &Ray3) -> Option<RaycastHit> {
-        let mut hit = None;
+        let mut hit: Option<RaycastHit> = None;
         for poly in &self.polygons {
             let poly_hit = poly.raycast(ray);
             if let Some(poly_hit) = poly_hit {
@@ -207,6 +282,27 @@ impl Shape for Mesh {
     }
 
     fn signed_distance(&self, pt: Pt3) -> f64 {
-        unimplemented!()
+        let mut distance = INFINITY;
+        for poly in &self.polygons {
+            let dist = poly.signed_distance(pt);
+            if dist < distance {
+                distance = dist;
+            }
+        }
+        distance
+    }
+}
+
+fn min_positive(a: f64, b: f64) -> f64 {
+    if a < 0.0 {
+        return b;
+    }
+    if b < 0.0 {
+        return a;
+    }
+    if a < b {
+        a
+    } else {
+        b
     }
 }
